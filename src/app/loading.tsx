17896d875eb04f773645b1935a866973578e1797
alessandro dominici <alessandrodominici@giu.local>
2025-10-16 00:04:59 +0200

    loadingPage

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,7 +1,31 @@
+import Image from "next/image";
+import { CSSProperties } from "react";
+import loadingStyle from "./loading.module.css";
+
+interface css {
+  divGen: CSSProperties;
+}
+
 export default function Loading() {
   return (
-    <>
-      <h1>Loading</h1>
-    </>
+    <div style={styles.divGen}>
+      <Image
+        src="/images/Logo.png"
+        alt="logo Loading"
+        width={200}
+        height={200}
+        className={loadingStyle.logo}
+      />
+    </div>
   );
 }
+
+const styles: css = {
+  divGen: {
+    display: "flex",
+    height: "100vh",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,25 @@
+"use client";
 import Main from "@/components/Main";
 import Navbar from "@/components/Navbar";
+import Loading from "./loading";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <div>
-      <Navbar />
-      <Main />
-    </div>
-  );
+  const [load, setLoad] = useState<boolean>(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoad(false);
+    }, 3000);
+  }, []);
+  if (load) {
+    return <Loading />;
+  } else {
+    return (
+      <div>
+        <Navbar />
+        <Main />
+      </div>
+    );
+  }
 }
