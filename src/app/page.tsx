ea847229a98edd6b29e64aabf4c6cadb031d94bc
alessandro dominici <alessandrodominici@giu.local>
2025-10-16 00:33:12 +0200

    loading and movile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,38 @@ import Main from "@/components/Main";
 import Navbar from "@/components/Navbar";
 import Loading from "./loading";
 import { useEffect, useState } from "react";
+import MainMobile from "@/components/mobile/MainMobile";
 
 export default function Home() {
   const [load, setLoad] = useState<boolean>(true);
+  const [isMobile, setIsMobile] = useState<boolean>(false);
 
+  //chequear si es movil o ordenador
+  useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 1000) {
+        setIsMobile(true);
+      } else {
+        setIsMobile(false);
+      }
+    };
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+  // retardar la carga para obtener mejores prestaciones
   useEffect(() => {
     setTimeout(() => {
       setLoad(false);
     }, 3000);
   }, []);
+
   if (load) {
     return <Loading />;
+  } else if (isMobile) {
+    return <MainMobile />;
   } else {
     return (
       <div>
---
+++ src/components/mobile/MainMobile.tsx
@@ -0,0 +1,8 @@
+import stylesCss from "./mainMovile.module.css";
+export default function MainMobile() {
+  return (
+    <div className={stylesCss.main}>
+      <div className={stylesCss.footer}></div>
+    </div>
+  );
+}
