c7d608ccb9f0dddeae106c0d8a02ff22279044c0
alessandro dominici <alessandrodominici@giu.local>
2026-09-14 15:51:31 +0200

    reset project

---
+++ .netlify/functions-internal/___netlify-server-handler/.next/server/middleware-build-manifest.js
@@ -0,0 +1,20 @@
+globalThis.__BUILD_MANIFEST = {
+  "pages": {
+    "/_app": []
+  },
+  "devFiles": [],
+  "polyfillFiles": [
+    "static/chunks/a6dad97d9634a72d.js"
+  ],
+  "lowPriorityFiles": [],
+  "rootMainFiles": [
+    "static/chunks/6426e5eb589292bd.js",
+    "static/chunks/d4b4a1ad90e2acdb.js",
+    "static/chunks/0b2c3a34a14dac7d.js",
+    "static/chunks/turbopack-742eaad3fe000b75.js"
+  ]
+};
+globalThis.__BUILD_MANIFEST.lowPriorityFiles = [
+"/static/" + process.env.__NEXT_BUILD_ID + "/_buildManifest.js",
+"/static/" + process.env.__NEXT_BUILD_ID + "/_ssgManifest.js"
+];
---
+++ .netlify/functions-internal/___netlify-server-handler/.next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"XVZmCyLcyD7kxrCyNd+ZoE3VNv5p4lBrjMJaB8JKCA4=\"\n}"
